fa6cfac82689e77542697fc5d26aa5a0a5e7779a
Your Name <you@example.com>
2023-02-22 08:57:10 -0500

    cambios

--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -1,6 +1,36 @@
-//definiendo datos
-let nombre= "James Rodriguez"
-if(5>2){
-    let nombre="Lucho Diaz"
-}
-console.log("hola "+nombre)
+import {productos} from '../helpers/baseDatos.js'
+
+
+//llamando a la fila
+let fila=document.getElementById("fila")
+
+//recorriendo el arreglo
+productos.forEach(function(producto){
+    
+    //columnas para los productos
+
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //crear tarjeta para almacenar la info de producto
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow")
+
+    //cresr una imagen 
+    let imagen=document.createElement("img")
+    imagen.classList.add("img-fluid", "w-100")
+    imagen.src=producto.fotos[0]
+
+    //padres e hijos
+
+    tarjeta.appendChild(imagen)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
+
+
+
+
+
+
+})
---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,40 @@
+//configurar una base de datos 
+//datos primitivos
+
+export let productos=Array(
+    {//atributo
+        nombre:"Producto 1",
+        descripcion:"aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/a1.webp?alt=media&token=e39ab483-9145-4e92-be00-762050b346b8","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 2",
+        descripcion:"aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/b1.webp?alt=media&token=f3d141a2-6ec8-464b-a145-5a5ffc0f215c","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 3",
+        descripcion:"aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/c1.webp?alt=media&token=c60920b5-c6a0-4815-bcf6-92d86ae4e664","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 4",
+        descripcion:"aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/d1.webp?alt=media&token=e1413926-d1a2-4c2f-a7f0-01b6dbcd4aad","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"Producto 5",
+        descripcion:"aqui ponga descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/e1.webp?alt=media&token=8ba4460d-8522-4e22-8162-fe54348d0fa2","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+)
