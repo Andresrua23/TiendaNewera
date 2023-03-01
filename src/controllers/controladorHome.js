26ae82f9caa78f5a061a2b45e6c499333230bbc0
Your Name <you@example.com>
2023-03-01 08:52:13 -0500

    Se creo plantilla

--- src/controllers/controladorHome.js
+++ src/controllers/controladorHome.js
@@ -1,36 +1,9 @@
 import {productos} from '../helpers/baseDatos.js'
+import { pintarProductos } from '../helpers/pintarProductos.js'
 
 
 //llamando a la fila
 let fila=document.getElementById("fila")
 
-//recorriendo el arreglo
-productos.forEach(function(producto){
-    
-    //columnas para los productos
-
-    let columna=document.createElement("div")
-    columna.classList.add("col")
-
-    //crear tarjeta para almacenar la info de producto
-    let tarjeta=document.createElement("div")
-    tarjeta.classList.add("card","h-100","shadow")
-
-    //cresr una imagen 
-    let imagen=document.createElement("img")
-    imagen.classList.add("img-fluid", "w-100")
-    imagen.src=producto.fotos[0]
-
-    //padres e hijos
-
-    tarjeta.appendChild(imagen)
-    columna.appendChild(tarjeta)
-    fila.appendChild(columna)
-
-
-
-
-
-
-
-})
+//pintar los productos de la tienda
+pintarProductos(productos)
--- src/helpers/baseDatos.js
+++ src/helpers/baseDatos.js
@@ -5,35 +5,35 @@ export let productos=Array(
     {//atributo
         nombre:"Producto 1",
         descripcion:"aqui ponga descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/a1.webp?alt=media&token=e39ab483-9145-4e92-be00-762050b346b8","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/a1.webp?alt=media&token=e39ab483-9145-4e92-be00-762050b346b8","https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/a2.webp?alt=media&token=23daecb9-47d0-4b5a-90cb-43e577742030"],
         precio:70000,
         popularidad:4.2
     },
     {
         nombre:"Producto 2",
         descripcion:"aqui ponga descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/b1.webp?alt=media&token=f3d141a2-6ec8-464b-a145-5a5ffc0f215c","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/b1.webp?alt=media&token=f3d141a2-6ec8-464b-a145-5a5ffc0f215c","https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/b2.webp?alt=media&token=02e3eca5-e20d-44e1-89b3-7bfba0d2529f"],
         precio:70000,
         popularidad:4.2
     },
     {
         nombre:"Producto 3",
         descripcion:"aqui ponga descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/c1.webp?alt=media&token=c60920b5-c6a0-4815-bcf6-92d86ae4e664","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/c1.webp?alt=media&token=c60920b5-c6a0-4815-bcf6-92d86ae4e664","https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/c2.webp?alt=media&token=7e12f47e-0252-4ba4-a4a6-73153aa74e30"],
         precio:70000,
         popularidad:4.2
     },
     {
         nombre:"Producto 4",
         descripcion:"aqui ponga descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/d1.webp?alt=media&token=e1413926-d1a2-4c2f-a7f0-01b6dbcd4aad","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/d1.webp?alt=media&token=e1413926-d1a2-4c2f-a7f0-01b6dbcd4aad","https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/d2.webp?alt=media&token=c1d3f446-b673-4db7-8cf5-457d19be853e"],
         precio:70000,
         popularidad:4.2
     },
     {
         nombre:"Producto 5",
         descripcion:"aqui ponga descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/e1.webp?alt=media&token=8ba4460d-8522-4e22-8162-fe54348d0fa2","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/e1.webp?alt=media&token=8ba4460d-8522-4e22-8162-fe54348d0fa2","https://firebasestorage.googleapis.com/v0/b/aratienda-360b4.appspot.com/o/e2.webp?alt=media&token=ecf5a189-1819-4b24-a21f-548f16210ed5"],
         precio:70000,
         popularidad:4.2
     },
